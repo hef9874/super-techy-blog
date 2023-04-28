8be7a853ce754cab281744cc11aa21c02aeb16da
nah <hef9874@gmail.com>
2023-04-28 15:02:25 -0400

    Add router post to user-routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,73 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+    try{
+        const users = await User.findAll();
+        res.status(200).json(err);
+    }
+    catch(err) {
+        res.status(500).json(err);
+    } 
+});
+
+router.get('/:id', async(req, res) => {
+    try{
+        const userDb = await User.findOne({
+            attributes: { exclude: ['password']},
+            where: {
+                id: req.params.id,
+            }, 
+                include: [
+                    {
+                        model: Post, 
+                        attributes: [ 'id', 'title', 'post_text', 'created_at']
+                    },
+                    {
+                    model: Comment, 
+                    attributes:  [ 'id', 'comment_text', 'created_at'],
+                    include: {
+                        model: Post,
+                        attributes: ['title']
+                    }
+                    }
+                ]
+        });
+        if (!userDb) {
+            return res.status(404).json({ message: 'There is no user with that ID' });
+        }
+        return res.json(userDb);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
+});
+
+router.post('/', (req, res) => {
+    try {
+        const userDb = await User.findOne({
+            where: {
+                email: req.body.email,
+            }
+        });
+        if(!userDb) {
+            res.status(400).json({ message: 'There is no user with that email'});
+            return;
+        }
+        const rightPassword = await userDb.checkPassword(req.body.password);
+
+        if(!rightPassword) {
+            res.status(400).json({ message: 'Wrong password'});
+            return;
+        }
+        req.session.user_id = userDb.id;
+        req.session.username = userDb.username;
+        req.session.loggedIn = true;
+
+        res.json({ user: userDb, message: 'Logged in' });
+    } catch(err){
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
